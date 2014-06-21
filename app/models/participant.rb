3c99696673efe9e283fa13022737bf631a5fe5d5
Justin Stewart <email": null>
2014-06-21 15:09:45 -0500

    tally guest and add styles to edit and destroy buttons

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -2,4 +2,17 @@ class Participant < ActiveRecord::Base
   belongs_to :events
   accepts_nested_attributes_for :events, allow_destroy: true
 
+  def popularity_label
+    if guests < 2
+      ""
+    elsif guests < 5
+    "Friendly"
+    elsif guests < 10
+      "Popular"
+    elsif guests < 25
+      "Social Butterfly"
+    else
+      "Pop Icon"
+    end
+  end
 end
