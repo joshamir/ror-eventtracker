7a5ad344ab0122776b30089079c9333080b10717
Justin Stewart <email": null>
2014-06-21 11:54:21 -0500

    Add page to add new events via form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,24 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+  def show
+
+  end
+  def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      redirect_to events_url
+    else
+      render action: "new"
+    end
+  end
+  private
+    def event_params
+      params.require(:event).permit(:name, :description, :location)
+    end
 end
